83a3f46fcad32293cb6f47f61cc01b977f547436
Bruce M. Axtens <bugmagnet@outlook.com>
2019-12-02 16:56:19 +0800

    Add project files.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+
+namespace XMerge
+{
+    public static class ExtensionMethods
+    {
+        public static XElement FindOrAddElement(this XContainer xml, string nodeName)
+        {
+            var node = xml.Descendants().FirstOrDefault(x => x.Name == nodeName);
+            if (node == null)
+                xml.Add(new XElement(nodeName));
+            return xml.Descendants().FirstOrDefault(x => x.Name == nodeName);
+        }
+
+        //public static string GetPath(this XElement node)
+        //{
+        //    string path = node.Name.ToString();
+        //    XElement currentNode = node;
+        //    while (currentNode.Parent != null)
+        //    {
+        //        currentNode = currentNode.Parent;
+        //        path = currentNode.Name.ToString() + @"\" + path;
+        //    }
+        //    return path;
+        //}
+
+        public static string GetPath(this XElement element)
+        {
+            return string.Join("/", element.AncestorsAndSelf().Reverse()
+                .Select(e =>
+                {
+                    var index = GetIndex(e);
+
+                    if (index == 1)
+                    {
+                        return e.Name.LocalName;
+                    }
+
+                    return string.Format("{0}[{1}]", e.Name.LocalName, GetIndex(e));
+                }));
+
+        }
+
+        private static int GetIndex(XElement element)
+        {
+            var i = 1;
+
+            if (element.Parent == null)
+            {
+                return 1;
+            }
+
+            foreach (var e in element.Parent.Elements(element.Name.LocalName))
+            {
+                if (e == element)
+                {
+                    break;
+                }
+
+                i++;
+            }
+
+            return i;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml.XPath;
+
+namespace XMerge
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Needs path to xml files");
+                return 1;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Needs path to xml files that's really there!");
+                return 2;
+            }
+
+            var xmls = Directory.GetFiles(args[0]);
+            var firstXml = xmls[0];
+            Console.WriteLine(firstXml);
+            var master = XDocument.Parse(File.ReadAllText(firstXml));
+            master = Prefill(master);
+            for (var i = 1; i < xmls.Length; i++)
+            {
+                var nextXml = xmls[i];
+                Console.WriteLine($"\t{nextXml}");
+                var next = XDocument.Parse(File.ReadAllText(nextXml));
+                master = Merge(master, next);
+                master = Prefill(master);
+            }
+
+            File.WriteAllText($"{(args.Length > 1 ? args[1] : "master")}.xml", master.Document.ToString());
+
+            return 0;
+        }
+
+        private static XDocument Prefill(XDocument master)
+        {
+            foreach (var node in from node in (from node in master.Descendants()
+                                               where node.DescendantNodes().Count() == 1
+                                               select node)
+                                 where !node.HasAttributes
+                                 select node)
+            {
+                node.SetAttributeValue("maxlength", node.Value.Length);
+                if (int.TryParse(node.Value, out int len))
+                {
+                    node.SetAttributeValue("type", len.GetType().Name);
+                }
+                else
+                {
+                    node.SetAttributeValue("type", node.Value.GetType().Name);
+                }
+            }
+
+            return master;
+        }
+
+        private static XDocument Merge(XDocument master, XDocument next)
+        {
+            var nodes = from node in next.Descendants() select node;
+            foreach (var node in nodes)
+            {
+                var path = node.GetPath();
+                var there = master.XPathSelectElement(path);
+                if (there == null)
+                {
+                    // add it somehow
+                    var pathParts = path.Split('/').ToList();
+                    pathParts.RemoveAt(pathParts.Count - 1);
+                    path = string.Join("/", pathParts.ToArray());
+                    there = master.XPathSelectElement(path);
+                    there.Add(node);
+                }
+                else
+                {
+                    var attribs = from attr in there.Attributes() where attr.Name == "maxlength" select attr;
+                    if (there.HasAttributes && attribs.Count() > 0)
+                    {
+                        var nlength = node.Value.Length; // int.Parse(there.Attribute("maxlength").Value);
+                        var mlength = int.Parse(there.Attribute("maxlength").Value);
+                        if (nlength > mlength)
+                        {
+                            there.SetAttributeValue("maxlength", nlength);
+                        }
+                    }
+                }
+            }
+
+            return master;
+        }
+
+    }
+}
